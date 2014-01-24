b3fdf213e8126e2ead05dc94d67b74d4b7662297
Viktor <viktorjordan@online.de>
2014-01-24 11:51:02 +0100

    added a simple controller

---
+++ PacMan/src/pacman/controllers/MyController.java
@@ -0,0 +1,63 @@
+package pacman.controllers;
+
+import pacman.game.Constants.MOVE;
+import pacman.game.Game;
+
+public class MyController extends Controller<MOVE> {
+
+	private LinearFunction valueFunction;
+	
+	/**
+	 * create a new controller
+	 */
+	public MyController(float[] coefficients) {
+		valueFunction = new LinearFunction(coefficients);
+	}
+	
+	@Override
+	public MOVE getMove(Game game, long timeDue) {
+		int currentNode = game.getPacmanCurrentNodeIndex();
+		MOVE lastMove = game.getPacmanLastMoveMade();
+		
+		MOVE bestMove = MOVE.NEUTRAL;
+		float bestMoveValueEstimation = Float.NEGATIVE_INFINITY;
+		
+		if (game.getNeighbour(currentNode, lastMove) != -1) {
+			bestMove = lastMove;
+			bestMoveValueEstimation = getValueFunctionEstimation(FeatureUtils.getFeatures(game, currentNode, lastMove));
+		}
+		
+		for (MOVE move : game.getPossibleMoves(game.getPacmanCurrentNodeIndex())) {
+			float[] features = FeatureUtils.getFeatures(game, game.getPacmanCurrentNodeIndex(), move);
+			float estimation = getValueFunctionEstimation(features);
+			if (bestMoveValueEstimation < estimation) {
+				bestMoveValueEstimation = estimation;
+				bestMove = move;
+			}
+		}
+		
+		return bestMove;
+	}
+	
+	/**
+	 * get the value estimation for the given state
+	 */
+	public float getValueFunctionEstimation(float[] input) {
+		return valueFunction.getOutput(input);
+	}
+	
+	/**
+	 * get the coefficients used for the value function
+	 */
+	public float[] getValueFunctionCoefficients() {
+		return valueFunction.getCoefficients();
+	}
+	
+	/**
+	 * set the coefficients used for the value function
+	 */
+	public void setValueFunctionCoefficients(float[] coefficients) {
+		valueFunction.setCoefficients(coefficients);
+	}
+
+}
